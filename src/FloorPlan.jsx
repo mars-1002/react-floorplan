b94a0f39548b06e9a45de9d4957729b416fa1530
mars-1002 <matthewabsalonramirez@gmail.com>
2023-05-13 18:04:23 -0400

    add all components

---
+++ src/FloorPlan.jsx
@@ -0,0 +1,16 @@
+//imported components
+import Kitchen from './Kitchen'
+import LivingRoom from './LivingRoom'
+import Bedroom from './Bedroom'
+import Bath from './Bath'
+import Oven from './Oven'
+import Sink from './Sink'
+
+const FloorPlan = (props) => {
+
+  return ( 
+    
+  );
+}
+
+export default FloorPlan;
